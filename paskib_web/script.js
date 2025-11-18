1bbd13466dc0754f27d13ef2896334d3d8d3979a
anjanirayya <rayyaandjani.com>
2025-11-18 19:26:40 +0700

    up section

--- paskib_web/script.js
+++ paskib_web/script.js
@@ -11,3 +11,19 @@ const observer = new IntersectionObserver((entries) => {
 
 // Seleksi semua elemen dengan data-animate
 document.querySelectorAll("[data-animate]").forEach((el) => observer.observe(el));
+
+// =================== LIGHTBOX GALERI ===================
+const galleryItems = document.querySelectorAll(".gallery-item img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+galleryItems.forEach(img => {
+    img.addEventListener("click", () => {
+        lightbox.style.display = "flex";
+        lightboxImg.src = img.src;
+    });
+});
+
+lightbox.addEventListener("click", () => {
+    lightbox.style.display = "none";
+});
