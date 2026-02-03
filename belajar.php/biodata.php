0cec6622f32aa6740d1788ceab3de2bff28feb72
anjanirayya <rayyaandjani.com>
2026-02-03 18:08:57 +0700

    biodata variable

--- belajar.php/biodata.php
+++ belajar.php/biodata.php
@@ -1,5 +1,23 @@
 <?php
-    echo "Nama: Rayya Andjani <br>" ;
-    echo "Kelas: XI TJKT 2 <br>" ;
-    echo "TTL: Bandung, 09 Juli 2009 <br>" ;
+
+// tipe data char (karakter)
+$jenis_kelamin = 'P';
+
+// tipe data string (teks)
+$nama_lengkap = "Rayya Andjani";
+
+// tipe data integer
+$umur = 16;
+
+// tipe data float
+$berat = 35;
+
+// tipe data float
+$tinggi = 145;
+
+echo "Nama: $nama_lengkap<br>";
+echo "Jenis Kelamin: $jenis_kelamin<br>";
+echo "Umur: $umur tahun<br>";
+echo "berat badan: $berat kg<br>";
+echo "tinggi badan: $tinggi cm<br>";
 ?>
